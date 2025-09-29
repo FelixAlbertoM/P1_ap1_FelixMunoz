2d16fe3eef74363559be24dfc1f042436f8bca4a
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-29 10:23:18 -0400

    creando el service solo con el listar

---
+++ Services/Servicios.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_ap1_FelixMunoz.DAL;
+using P1_ap1_FelixMunoz.Models;
+using System.Linq.Expressions;
+
+namespace P1_ap1_FelixMunoz.Services;
+
+public class Servicios(IDbContextFactory<Contexto> contextFactory)
+{
+
+    public async Task<List<Registros>> Listar(Expression<Func<Registros, bool>> criterio)
+    {
+        await using var contexto = await contextFactory.CreateDbContextAsync();
+        return await contexto.Registros.Where(criterio).AsNoTracking().ToListAsync();
+    }
+
+}
