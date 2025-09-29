ac44a6d5144397d74c0aebaa5998b81d69e5e5b1
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-29 10:08:39 -0400

    creando el modelo

---
+++ Models/Registros.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P1_ap1_FelixMunoz.Models;
+
+public class Registros
+{
+    [Key]
+    public int RegistroId { get; set; }
+
+    [Required(ErrorMessage = "El campo Registro es obligatorio.")]
+    public string Registro { get; set; } = null;
+
+}
