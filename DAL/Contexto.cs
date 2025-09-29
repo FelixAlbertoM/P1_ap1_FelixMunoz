b29be794db9289bd18ac522f724a014ae905f017
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-29 10:12:24 -0400

    creando el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_ap1_FelixMunoz.Models;
+
+namespace P1_ap1_FelixMunoz.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Registros> Registros { get; set; }
+
+}
+
