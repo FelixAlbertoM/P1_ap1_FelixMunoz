cea9e74a77d7002b81ffd65832b4da60e96caacd
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-09-29 20:15:39 -0400

    arreglando el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -7,7 +7,7 @@ public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
-    public DbSet<EntradasHuacales> EntradaHuacales { get; set; }
+    public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
 
 }
 
