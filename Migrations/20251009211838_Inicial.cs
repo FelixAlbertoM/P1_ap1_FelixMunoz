5077a2936b6726c7cc0910815763fa0c2664d5b3
Felix Alberto Munoz <papoelnecio30@gmail.com>
2025-10-10 10:25:09 -0400

    haciendo la migracion

---
+++ Migrations/20251009211838_Inicial.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace P1_ap1_FelixMunoz.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntradasHuacales",
+                columns: table => new
+                {
+                    IdEntrada = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    NombreCliente = table.Column<string>(type: "TEXT", nullable: false),
+                    Cantidad = table.Column<double>(type: "REAL", nullable: false),
+                    Precio = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradasHuacales", x => x.IdEntrada);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TiposHuacales",
+                columns: table => new
+                {
+                    IdTipo = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Existencia = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposHuacales", x => x.IdTipo);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntradasHuacalesDetalle",
+                columns: table => new
+                {
+                    detalleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    IdEntrada = table.Column<int>(type: "INTEGER", nullable: false),
+                    IdTipo = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cantidad = table.Column<int>(type: "INTEGER", nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradasHuacalesDetalle", x => x.detalleId);
+                    table.ForeignKey(
+                        name: "FK_EntradasHuacalesDetalle_EntradasHuacales_IdEntrada",
+                        column: x => x.IdEntrada,
+                        principalTable: "EntradasHuacales",
+                        principalColumn: "IdEntrada",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EntradasHuacalesDetalle_TiposHuacales_IdTipo",
+                        column: x => x.IdTipo,
+                        principalTable: "TiposHuacales",
+                        principalColumn: "IdTipo",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TiposHuacales",
+                columns: new[] { "IdTipo", "Descripcion", "Existencia" },
+                values: new object[,]
+                {
+                    { 1, "Huacal Rojo - Grande", 100 },
+                    { 2, "Huacal verde - Grande", 150 },
+                    { 3, "Huacal verde - Pequeña", 200 },
+                    { 4, "Huacal Rojo - Pequeña", 0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasHuacalesDetalle_IdEntrada",
+                table: "EntradasHuacalesDetalle",
+                column: "IdEntrada");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasHuacalesDetalle_IdTipo",
+                table: "EntradasHuacalesDetalle",
+                column: "IdTipo");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntradasHuacalesDetalle");
+
+            migrationBuilder.DropTable(
+                name: "EntradasHuacales");
+
+            migrationBuilder.DropTable(
+                name: "TiposHuacales");
+        }
+    }
+}
